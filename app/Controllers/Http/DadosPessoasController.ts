21bb8656b831a7af69e5eb63d2ad2acf368fd2d6
Eduardo Garcia <edusantosgarcia@gmail.com>
2021-11-17 00:20:13 -0300

    swagger

--- app/Controllers/Http/DadosPessoasController.ts
+++ app/Controllers/Http/DadosPessoasController.ts
@@ -1,3 +1,9 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class DadosPessoasController {}
+export default class DadosPessoasController {
+    
+    public async hello({ request, response }: HttpContextContract) {
+        const name = request.input('name', 'Guess')
+        return response.send({ message: 'Hello ' + name })
+    }
+}
